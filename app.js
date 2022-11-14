091e3aab5adb87bfa30fbb839c1784b017097997
Alberto Alvarez <elbeto80@hotmail.com>
2022-11-14 12:34:14 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log("corriendo app " + port);
+});
