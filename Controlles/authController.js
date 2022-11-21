53513e36b4ce38268c6b59c6f9bf5eca1b9b21f5
Alberto Alvarez <elbeto80@hotmail.com>
2022-11-20 19:03:55 -0500

    Login user

--- Controlles/authController.js
+++ Controlles/authController.js
@@ -1,7 +1,7 @@
 const { userModel } = require("../Models");
 const { matchedData } = require("express-validator");
 
-const { encrypt } = require("../Global/Password");
+const { encrypt, compare } = require("../Global/Password");
 
 const { tokenCreate } = require("../Global/JWT_util");
 
@@ -30,7 +30,33 @@ const LoginUser = async (req, res) => {
   try {
     req = matchedData(req);
 
-    return res.status(200).json({ req });
+    const user = await userModel
+      .findOne({ email: req.email })
+      .select("name email role password");
+    if (!user) {
+      return res
+        .status(403)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const checkUser = await compare(req.password, user.password);
+    if (!checkUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid password" });
+    }
+
+    const response = {
+      user: {
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      token: await tokenCreate(user),
+    };
+
+    return res.status(200).json({ data: response });
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
