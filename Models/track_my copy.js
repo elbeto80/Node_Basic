595863a599712a2d29f6aa833264b2b53472dfe6
Alberto Alvarez <elbeto80@hotmail.com>
2022-11-26 17:25:45 -0500

    Crea Modelos mysql

---
+++ Models/track_my copy.js
@@ -0,0 +1,41 @@
+const { sequelize } = (require = "../Config/mysql_db.js");
+const { DataTypes } = require("sequelize");
+
+const Track = sequelize.define(
+  "tracks",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    album: {
+      type: DataTypes.STRING,
+    },
+    cover: {
+      type: DataTypes.STRING,
+    },
+    artist_name: {
+      type: DataTypes.STRING,
+    },
+    artist_nickname: {
+      type: DataTypes.STRING,
+    },
+    artist_nationality: {
+      type: DataTypes.STRING,
+    },
+    duration_start: {
+      type: DataTypes.INTEGER,
+    },
+    duration_end: {
+      type: DataTypes.INTEGER,
+    },
+    mediaId: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Track;
---
+++ Models/user_my.js
@@ -0,0 +1,18 @@
+const { sequelize } = (require = "../Config/mysql_db.js");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define(
+  users,
+  {
+    name: { type: DataTypes.STRING, allowNull: false },
+    age: { type: DataTypes.NUMBER },
+    email: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.ENUM(["user", "admin"]) },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = User;
